43aa85c5f9e2e5ec64fd09fea53526b2c3e30428
Sam Matteson <sammatt@nuevaschool.org>
2026-01-10 21:35:02 -0800

    rm CmdUtils.nameCommand() - use command.withName() instead

--- src/main/java/org/usfirst/frc4904/robot/CmdUtils.java
+++ src/main/java/org/usfirst/frc4904/robot/CmdUtils.java
@@ -5,11 +5,6 @@ import org.usfirst.frc4904.standard.commands.NoOp;
 
 public class CmdUtils {
 
-    public static Command nameCommand(String name, Command cmd) {
-        cmd.setName(name);
-        return cmd;
-    }
-
     public static Command nonNull(Command cmd) {
         return cmd == null ? new NoOp() : cmd;
     }
