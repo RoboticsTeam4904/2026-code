5b9ce334bce4ee0fe815cc8ab24fa3359d4f5a01
Ravi <r2a3v5i7.kumar@gmail.com>
2026-01-11 09:53:03 -0800

    remove cursed autons

--- src/main/java/org/usfirst/frc4904/robot/Auton.java
+++ src/main/java/org/usfirst/frc4904/robot/Auton.java
@@ -31,22 +31,7 @@ public class Auton {
         return Component.chassis.getAutonomousCommand("straight", true, false);
     }
 
-     /**
-     * Move straight out of the starting zone, align with the center of the reef, and outtake.
-     * PUTS THE CORAL ON THE ALGAE :skull:
-     */
-    public static Command c_jankStraightCoral() {
-        return new SequentialCommandGroup(
-            new WaitCommand(5),
-            Component.chassis.c_driveRobotRelative(0.4, 0, 0).withTimeout(2),
-            Component.chassis.c_stop(),
-            Component.vision.c_align(new int[] { 10, 21 }, -1),
-            Component.elevator.c_outtakeAtPosition(ElevatorSubsystem.Position.L3),
-            Component.elevator.c_gotoPosition(ElevatorSubsystem.Position.INTAKE)
-        );
-    }
-
-    // actually moves backwards - robot must be placed physically backwards on the field
+   // actually moves backwards - robot must be placed physically backwards on the field
     public static Command c_jankStraight() {
         return new SequentialCommandGroup(
             new WaitCommand(12),
@@ -70,62 +55,5 @@ public class Auton {
 
     public static Command c_jankRightCoral() {
         return c_jankSideCoral(-1);
-    }
-
-    /** @param side 1 = robot-relative left, -1 = robot-relative right */
-    private static Command c_jankSideCoral(int side) {
-        return new SequentialCommandGroup(
-            new WaitCommand(3),
-            Component.chassis.c_driveRobotRelative(-0.4 / Math.sqrt(3), -0.4 * side, 0).withTimeout(1.5),
-            Component.chassis.c_stop(),
-            Component.vision.c_align(new int[] { 9, 11, 20, 22 }, -1),
-            Component.elevator.c_outtakeAtPosition(ElevatorSubsystem.Position.L2),
-            Component.elevator.c_gotoPosition(ElevatorSubsystem.Position.INTAKE)
-        );
-    }
-
-    // /**
-    //  * Move straight out of the starting zone, align with the center of the reef, and outtake.
-    //  */
-    // public static Command c_straightCoral() {
-    //     return new SequentialCommandGroup(
-    //         // Component.chassis.getAutonomousCommand("straight", true, getFlipAlliance(), false),
-    //         Component.vision.c_align(TagGroup.REEF_INNER_CENTER, -1),
-    //         Component.elevator.c_outtakeAtPosition(ElevatorSubsystem.Position.L2)
-    //     );
-    // }
-
-    /**
-     * Move diagonally from the starting zone, align with the inner diagonal of the reef, and outtake.
-     */
-    public static Command c_sideCoral() {
-        return new SequentialCommandGroup(
-            Component.chassis.getAutonomousCommand("side", true, getFlipSide()),
-            Component.vision.c_align(TagGroup.REEF_INNER_DIAGONAL, -1)
-            //Component.elevator.c_outtakeAtPosition(ElevatorSubsystem.Position.L2)
-        );
-    }
-
-    /**
-     * <ol>
-     *   <li> Move diagonally from the starting zone
-     *   <li> Align with the inner diagonal of the reef
-     *   <li> Outtake
-     *   <li> Move to and align with the coral intake station
-     *   <li> Wait 2 seconds for intake
-     *   <li> Move to and align with the outer diagonal of the reef
-     *   <li> Outtake
-     * </ol>
-     */
-    public static Command c_sideCoralFancy() {
-        return new SequentialCommandGroup(
-            c_sideCoral(),
-            Component.chassis.getAutonomousCommand("side2", true, getFlipSide()),
-            new WaitCommand(2),
-            //Component.elevator.c_intake(),
-            Component.chassis.getAutonomousCommand("side3", true, getFlipSide()),
-            Component.vision.c_align(TagGroup.REEF_OUTER_DIAGONAL, -1)
-            //Component.elevator.c_outtakeAtPosition(ElevatorSubsystem.Position.L2)
-        );
     }
 }
