44fbd66f96cf8da89e2fb6e184658524e391c7e2
SourDouh <josgoul@nuevaschool.org>
2026-01-16 17:06:15 -0800

    start of a broken subsystem

---
+++ src/main/java/org/usfirst/frc4904/robot/subsystems/ClimberSubsystem.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc4904.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.usfirst.frc4904.standard.custom.motorcontrollers.SmartMotorController;
+
+
+public class ClimberSubsystem extends MotorSubsystem {
+
+    
+}
