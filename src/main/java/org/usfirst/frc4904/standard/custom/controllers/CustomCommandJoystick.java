19501dd6328f53f91a3320259913cbd60787970e
Sam Matteson <sammatt@nuevaschool.org>
2026-01-17 23:42:21 -0800

    add method to clear all trigger bindings on CustomCommandJoystick

--- src/main/java/org/usfirst/frc4904/standard/custom/controllers/CustomCommandJoystick.java
+++ src/main/java/org/usfirst/frc4904/standard/custom/controllers/CustomCommandJoystick.java
@@ -1,35 +1,53 @@
 package org.usfirst.frc4904.standard.custom.controllers;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.event.EventLoop;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A joystick that implements the generic controller interface and the 2023
  * trigger interface. This allows us to use a joystick as a controller. This
  * contains 12 buttons to reflect the joysticks we are typically using.
- *
- * TO DO: should probably extend or be replaced with https://github.wpilib.org/allwpilib/docs/release/java/edu/wpi/first/wpilibj2/command/button/CommandJoystick.html
  */
-public class CustomCommandJoystick extends Joystick {
-    public static final int X_AXIS = 0;
-    public static final int Y_AXIS = 1;
-    public static final int SLIDER_AXIS = 3;
+public class CustomCommandJoystick extends CommandJoystick {
+
+    public enum Axis {
+        X(0),
+        Y(1),
+        SLIDER(3);
+
+        public final int axis;
+
+        Axis(int axis) {
+            this.axis = axis;
+        }
+    }
+
+    private final EventLoop loop;
+
     protected final double deadzone;
     protected final int port;
-    // Buttons
-    public final JoystickButton button1;
-    public final JoystickButton button2;
-    public final JoystickButton button3;
-    public final JoystickButton button4;
-    public final JoystickButton button5;
-    public final JoystickButton button6;
-    public final JoystickButton button7;
-    public final JoystickButton button8;
-    public final JoystickButton button9;
-    public final JoystickButton button10;
-    public final JoystickButton button11;
-    public final JoystickButton button12;
+
+    public final Trigger button1;
+    public final Trigger button2;
+    public final Trigger button3;
+    public final Trigger button4;
+    public final Trigger button5;
+    public final Trigger button6;
+    public final Trigger button7;
+    public final Trigger button8;
+    public final Trigger button9;
+    public final Trigger button10;
+    public final Trigger button11;
+    public final Trigger button12;
 
     public CustomCommandJoystick(int port, double deadzone) {
         super(port);
@@ -38,56 +56,68 @@ public class CustomCommandJoystick extends Joystick {
         }
         this.deadzone = deadzone;
         this.port = port;
-        button1  = new JoystickButton(this, 1);
-        button2  = new JoystickButton(this, 2);
-        button3  = new JoystickButton(this, 3);
-        button4  = new JoystickButton(this, 4);
-        button5  = new JoystickButton(this, 5);
-        button6  = new JoystickButton(this, 6);
-        button7  = new JoystickButton(this, 7);
-        button8  = new JoystickButton(this, 8);
-        button9  = new JoystickButton(this, 9);
-        button10 = new JoystickButton(this, 10);
-        button11 = new JoystickButton(this, 11);
-        button12 = new JoystickButton(this, 12);
+
+        loop = new EventLoop();
+        CommandScheduler.getInstance().getDefaultButtonLoop().bind(loop::poll);
+
+        button1  = button(1);
+        button2  = button(2);
+        button3  = button(3);
+        button4  = button(4);
+        button5  = button(5);
+        button6  = button(6);
+        button7  = button(7);
+        button8  = button(8);
+        button9  = button(9);
+        button10 = button(10);
+        button11 = button(11);
+        button12 = button(12);
     }
 
-    /**
-     * Returns true if a given axis is above the move threshold.
-     *
-     * @param axis
-     * @return	whether the current value of that axis is outside of the deadzone
-     */
-    public boolean active(int axis) {
-        return Math.abs(getAxis(axis)) > deadzone;
-        // if (axis == CustomCommandJoystick.X_AXIS) {
-        // 	return Math.abs(super.getX()) > deadzone;
-        // } else if (axis == CustomCommandJoystick.Y_AXIS) {
-        // 	return Math.abs(super.getY()) > deadzone;
-        // } else {
-        // 	return false;
-        // }
+    @Override
+    public Trigger button(int id) {
+        return super.button(id, loop);
+    }
+
+    @Override
+    public Trigger button(int button, EventLoop loop) {
+        // clearBindings() won't work if the button is on a different event loop
+        throw new UnsupportedOperationException("CustomCommandJoystick.button() does not support setting a custom event loop.");
+    }
+
+    public void clearBindings() {
+        loop.clear();
     }
 
     /**
      * Returns true if the joystick is actually connected. It determines this by
      * counting the number of buttons (more than 0 means the joystick is connected).
-     *
-     * @return
      */
     public boolean connected() {
         return DriverStation.getStickButtonCount(port) > 0;
     }
 
     /**
-     * Returns the value of the given axis.
+     * Returns the raw value of the given axis (no deadzone calculation).
      */
-    public double getAxis(int axis) {
-        double val = super.getRawAxis(axis);
-        if (Math.abs(val) < deadzone) {
-            return 0.0;
-        }
-        return (val - Math.signum(val) * deadzone) / (1 - deadzone); // linear between 0 and 1 in the remaining range
-        // return super.getRawAxis(axis);
+    public double getRawAxis(Axis axis) {
+        return super.getRawAxis(axis.axis);
+    }
+
+    /**
+     * Returns the value of the given axis, with the deadzone applied.
+     */
+    public double getAxis(Axis axis) {
+        return MathUtil.applyDeadband(getRawAxis(axis), deadzone);
+    }
+
+    /**
+     * Returns true if a given axis is above the move threshold.
+     *
+     * @param axis
+     * @return whether the current value of that axis is outside the deadzone
+     */
+    public boolean active(Axis axis) {
+        return Math.abs(getRawAxis(axis)) > deadzone;
     }
 }
