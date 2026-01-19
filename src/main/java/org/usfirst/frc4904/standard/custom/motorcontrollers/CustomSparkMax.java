0d9eeba568719194cb7d48497bb87fea2f129502
Sam Matteson <sammatt@nuevaschool.org>
2026-01-19 13:49:23 -0800

    add methods to SmartMotorController to use motor's built in pid/ff functionality
    make SmartMotorConfigSlot config methods chainable
    
    add continuous mode to SmartMotorConfigSlot
    
    use built-in motor pid for swerve
    
    # Conflicts:
    #	src/main/java/org/usfirst/frc4904/robot/swerve/SwerveModule.java
    
    remove explicit motor config slots, move cfg methods to SmartMotorController
    
    zeroing and other fixes

--- src/main/java/org/usfirst/frc4904/standard/custom/motorcontrollers/CustomSparkMax.java
+++ src/main/java/org/usfirst/frc4904/standard/custom/motorcontrollers/CustomSparkMax.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc4904.standard.custom.motorcontrollers;
 
+import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
@@ -33,22 +34,27 @@ public class CustomSparkMax extends SparkMax implements SmartMotorController {
         );
     }
 
-    public void setBrakeOnNeutral() {
+    public CustomSparkMax setBrakeOnNeutral() {
         configure(new SparkMaxConfig().idleMode(IdleMode.kBrake));
+        return this;
     }
 
-    public void setCoastOnNeutral() {
+    public CustomSparkMax setCoastOnNeutral() {
         configure(new SparkMaxConfig().idleMode(IdleMode.kCoast));
+        return this;
     }
+
+    private boolean inverted = false;
 
     @SuppressWarnings("deprecation") // overrides deprecated method
     public void setInverted(boolean inverted) {
+        this.inverted = inverted;
         configure(new SparkMaxConfig().inverted(inverted));
     }
 
-    @Override
-    public void neutralOutput() {
-        stopMotor();
+    @SuppressWarnings("deprecation") // overrides deprecated method
+    public boolean getInverted() {
+        return inverted;
     }
 
     @Override
@@ -61,5 +67,100 @@ public class CustomSparkMax extends SparkMax implements SmartMotorController {
     public boolean isRevLimitSwitchPressed() throws IllegalAccessException {
         if (!limitSwitch) throw new IllegalAccessException("Cannot read reverse limit switch state when CustomSparkMax was constructed without limit switch.");
         return getReverseLimitSwitch().isPressed();
+    }
+
+    private final SparkMaxConfig config = new SparkMaxConfig();
+
+    private double motorMechanismRatio = 1;
+    private double continuousRange = 0;
+
+    @Override
+    public double getRotation() {
+        // division is (in theory) handled by config.encoder.positionConversionFactor() below
+        return getAbsoluteEncoder().getPosition(); //  / motorMechanismRatio;
+    }
+
+    @Override
+    public CustomSparkMax setMotorMechanismRatio(double ratio) {
+        motorMechanismRatio = ratio;
+        double reciprocal = 1 / ratio;
+        config.closedLoop.feedForward.kCosRatio(reciprocal);  // TODO reciprocal maybe wrong here? - also maybe not necessary at all if line below affects this (though i doubt it)
+        config.encoder.positionConversionFactor(reciprocal);
+        config.encoder.velocityConversionFactor(reciprocal);
+        configure(config);
+        return this;
+    }
+
+    @Override
+    public double getMotorMechanismRatio() {
+        return motorMechanismRatio;
+    }
+
+    @Override
+    public CustomSparkMax setMechanismRotationOffset(double offset) {
+        throw new UnsupportedOperationException("CustomSparkMax does not support setMechanismRotationOffset().");
+    }
+
+    @Override
+    public double getMechanismRotationOffset() {
+        System.err.println("CustomSparkMax does not support getMechanismRotationOffset().");
+        return 0;
+    }
+
+    @Override
+    public CustomSparkMax setPID(double p, double i, double d) {
+        config.closedLoop.pid(p, i, d);
+        configure(config);
+        return this;
+    }
+
+    @Override
+    public CustomSparkMax setElevFF(double kS, double kG, double kV, double kA) {
+        config.closedLoop.feedForward.kCos(0);
+        config.closedLoop.feedForward.svag(kS, kV, kA, kG);
+        configure(config);
+        return this;
+    }
+
+    @Override
+    public CustomSparkMax setArmFF(double kS, double kG, double kV, double kA) {
+        config.closedLoop.feedForward.kG(0);
+        config.closedLoop.feedForward.kCos(kG);
+        config.closedLoop.feedForward.sva(kS, kV, kA);
+        configure(config);
+        return this;
+    }
+
+    @Override
+    public CustomSparkMax setContinuousRange(double range) {
+        if (range < 0) {
+            throw new IllegalArgumentException("CustomSparkMax.setContinuousRange() cannot be negative.");
+        }
+
+        continuousRange = range;
+        if (range != 0) {
+            config.closedLoop.positionWrappingEnabled(true);
+            config.closedLoop.positionWrappingInputRange(0, range);
+        } else {
+            config.closedLoop.positionWrappingEnabled(false);
+        }
+        return this;
+    }
+
+    @Override
+    public double getContinuousRange() {
+        return continuousRange;
+    }
+
+    @Override
+    public void holdPosition(double pos, double addedVoltage) {
+        // TODO should maybe be pos * motorMechanismRatio ?
+        getClosedLoopController().setSetpoint(pos, ControlType.kPosition, ClosedLoopSlot.kSlot0, addedVoltage);
+    }
+
+    @Override
+    public void holdVelocity(double vel, double addedVoltage) {
+        // TODO should maybe be vel * motorMechanismRatio ?
+        getClosedLoopController().setSetpoint(vel, ControlType.kVelocity, ClosedLoopSlot.kSlot0, addedVoltage);
     }
 }
