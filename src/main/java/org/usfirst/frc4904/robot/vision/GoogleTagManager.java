7dbcbf99ceaac7958cb9a4df64540a18908dc555
Ayush Kumar <github@ayukmr.com>
2026-01-18 21:28:02 -0800

    add documentation

--- src/main/java/org/usfirst/frc4904/robot/vision/GoogleTagManager.java
+++ src/main/java/org/usfirst/frc4904/robot/vision/GoogleTagManager.java
@@ -14,6 +14,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 
+/** Manages Google tags */
 public class GoogleTagManager {
     private final HttpClient client;
 
