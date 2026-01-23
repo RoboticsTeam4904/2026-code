8cedc3c46505e3afde7bdf40cdf05a41c4252341
Ayush Kumar <github@ayukmr.com>
2026-01-22 17:39:53 -0800

    update nt

--- src/main/java/org/usfirst/frc4904/robot/vision/GoogleTagManager.java
+++ src/main/java/org/usfirst/frc4904/robot/vision/GoogleTagManager.java
@@ -3,40 +3,50 @@ package org.usfirst.frc4904.robot.vision;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTableEntry;
 
 /** Manages Google tags */
 public class GoogleTagManager {
 
-    private final NetworkTable tagsTable;
+    private final NetworkTableEntry tagsEntry;
+    private final ObjectMapper mapper = new ObjectMapper();
 
     public record Tag(int id, Rotation2d rot, Translation3d pos, int camera) {}
 
     public GoogleTagManager() {
-        tagsTable = NetworkTableInstance.getDefault()
-            .getTable("dauntless")
-            .getSubTable("tags");
+        tagsEntry = NetworkTableInstance.getDefault().getEntry("/dauntless/tags");
     }
 
     public List<Tag> getTags() {
         List<Tag> tags = new ArrayList<>();
 
-        for (String key : tagsTable.getSubTables()) {
-            NetworkTable tagTable = tagsTable.getSubTable(key);
+        String json = tagsEntry.getString("");
+        if (json.isEmpty()) return tags;
 
-            int id = tagTable.getEntry("id").getNumber(-1).intValue();
-            double rot = tagTable.getEntry("rot").getDouble(0.0);
-            double[] pos = tagTable.getEntry("pos").getDoubleArray(new double[0]);
+        try {
+            JsonNode root = mapper.readTree(json);
 
-            tags.add(new Tag(
-                id,
-                Rotation2d.fromRotations(rot),
-                new Translation3d(pos[2], pos[0], pos[1]),
-                0
-            ));
+            for (JsonNode el : root) {
+                JsonNode idPath = el.path("id");
+                if (idPath.isNull()) continue;
+
+                double[] pos = mapper.treeToValue(el.path("pos"), double[].class);
+
+                tags.add(new Tag(
+                    idPath.asInt(),
+                    Rotation2d.fromRotations(el.path("rot").asDouble()),
+                    new Translation3d(pos[2], pos[0], pos[1]),
+                    0
+                ));
+            }
+        } catch (Exception e) {
+            System.out.println("google tag manager parsing error!!!");
         }
 
         return tags;
