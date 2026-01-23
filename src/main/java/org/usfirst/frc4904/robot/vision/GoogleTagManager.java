6530c30bfc31d7f20076ae7834241f1b66a05491
Ayush Kumar <github@ayukmr.com>
2026-01-22 17:20:55 -0800

    use network tables

--- src/main/java/org/usfirst/frc4904/robot/vision/GoogleTagManager.java
+++ src/main/java/org/usfirst/frc4904/robot/vision/GoogleTagManager.java
@@ -1,68 +1,42 @@
 package org.usfirst.frc4904.robot.vision;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 /** Manages Google tags */
 public class GoogleTagManager {
 
-    private final HttpClient client;
+    private final NetworkTable tagsTable;
 
     public record Tag(int id, Rotation2d rot, Translation3d pos, int camera) {}
 
     public GoogleTagManager() {
-        client = HttpClient.newHttpClient();
+        tagsTable = NetworkTableInstance.getDefault()
+            .getTable("dauntless")
+            .getSubTable("tags");
     }
 
     public List<Tag> getTags() {
-        HttpRequest request =
-            HttpRequest.newBuilder()
-                .uri(URI.create("http://10.49.4.203:8000/api/tags"))
-                .GET()
-                .build();
-
         List<Tag> tags = new ArrayList<>();
 
-        String json;
+        for (String key : tagsTable.getSubTables()) {
+            NetworkTable tagTable = tagsTable.getSubTable(key);
 
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            json = response.body();
-        } catch (IOException | InterruptedException e) {
-            System.out.println("google tag manager fetching error!!!\n" + e.getClass().getName() + ": " + e.getMessage());
-            return tags;
-        }
+            int id = tagTable.getEntry("id").getNumber(-1).intValue();
+            double rot = tagTable.getEntry("rot").getDouble(0.0);
+            double[] pos = tagTable.getEntry("pos").getDoubleArray(new double[0]);
 
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode root = mapper.readTree(json);
-
-            for (JsonNode el : root) {
-                JsonNode idPath = el.path("id");
-                if (idPath.isNull()) continue; // skip tags with no id
-
-                double[] pos = mapper.treeToValue(el.path("pos"), double[].class);
-
-                tags.add(new Tag(
-                    idPath.asInt(),
-                    Rotation2d.fromRotations(el.path("rot").asDouble()),
-                    new Translation3d(pos[2], pos[0], pos[1]),
-                    0
-                ));
-            }
-        } catch (Exception e) {
-            System.out.println("google tag manager parsing error!!!");
+            tags.add(new Tag(
+                id,
+                Rotation2d.fromRotations(rot),
+                new Translation3d(pos[2], pos[0], pos[1]),
+                0
+            ));
         }
 
         return tags;
