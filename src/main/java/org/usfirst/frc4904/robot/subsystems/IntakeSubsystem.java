5793927a7c49220328fe11978521b816600187c0
Lumi Yaida <lumiyaida@gmail.com>
2026-01-23 18:12:08 -0800

    intake subsystem added, working on

---
+++ src/main/java/org/usfirst/frc4904/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc4904.robot.subsystems;
+
+import org.usfirst.frc4904.standard.custom.motorcontrollers.SmartMotorController;
+
+public class IntakeSubsystem extends MotorSubsystem{
+    public IntakeSubsystem(SmartMotorController intakeVerticalMotor, SmartMotorController intakeRollerMotor, CustomEncoder intakeEncoder){
+        super(
+            new SmartMotorController[] {intakeVerticalMotor, intakeRollerMotor},
+            4
+        );
+    }
+
+    public Command c_verticalDown () {
+        return c_forward(true);
+    }
+
+    public Command c_verticalUp () {
+        return c_backward(true);
+    }
+}
