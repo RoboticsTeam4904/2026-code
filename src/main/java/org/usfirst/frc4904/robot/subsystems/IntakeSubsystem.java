359cd2a9482153e5c3820b619ec8cc3b393cb03a
Sam Matteson <sammatt@nuevaschool.org>
2026-01-28 18:16:14 -0800

    minor ezmotion rearrangement

--- src/main/java/org/usfirst/frc4904/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/org/usfirst/frc4904/robot/subsystems/IntakeSubsystem.java
@@ -3,12 +3,10 @@ package org.usfirst.frc4904.robot.subsystems;
 import org.usfirst.frc4904.standard.custom.motioncontrollers.ezControl;
 import org.usfirst.frc4904.standard.custom.motioncontrollers.ezMotion;
 import org.usfirst.frc4904.standard.custom.motorcontrollers.SmartMotorController;
-import org.usfirst.frc4904.standard.custom.sensors.CustomEncoder;
 
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.DutyCycle;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -59,45 +57,26 @@ public class IntakeSubsystem extends MotorSubsystem {
     private Command getRawAngleCommand(double angle) {
         ezControl controller = new ezControl(
             kP, kI, kD,
-            (position, velocityMetersPerSec) -> feedforward.calculate(getAngle(), velocityMetersPerSec)
+            (position, velocity) -> feedforward.calculate(getAngle(), velocity)
         );
 
         TrapezoidProfile profile = new TrapezoidProfile(
             new TrapezoidProfile.Constraints(MAX_VEL, MAX_ACCEL)
         );
 
-        Command cmd = getEzMotion(
-            controller,
-            profile,
-            new TrapezoidProfile.State(getAngle(), 0), // TODO why are we assuming the velocity is 0
-            new TrapezoidProfile.State(angle, 0)
-        );
-        cmd.setName("elevator - c_gotoAngle");
-        cmd.addRequirements(this);
-        return cmd;
-    }
+        var start = new TrapezoidProfile.State(getAngle(), 0);
+        var goal = new TrapezoidProfile.State(angle, 0);
 
-    private ezMotion getEzMotion(
-        ezControl controller,
-        TrapezoidProfile profile,
-        TrapezoidProfile.State current,
-        TrapezoidProfile.State goal
-    ) {
         return new ezMotion(
             controller,
             this::getAngle,
             this::setVoltage,
             (double t) -> {
-                TrapezoidProfile.State result = profile.calculate(t, current, goal);
-                return new Pair<>(result.position, result.velocity);
+                var setpoint = profile.calculate(t, start, goal);
+                return new Pair<>(setpoint.position, setpoint.velocity);
             },
             this
-        ) {
-            @Override
-            public void end(boolean interrupted) {
-                setVoltage(0);
-            }
-        };
+        ).finallyDo(this::stop);
     }
 
 }
