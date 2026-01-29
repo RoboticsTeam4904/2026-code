bb14edfe710b50ae44a79b725a7213f533264265
Robby20914 <sofia.robison@gmail.com>
2026-01-28 17:33:30 -0800

    added some shooter things

--- src/main/java/org/usfirst/frc4904/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/org/usfirst/frc4904/robot/subsystems/ShooterSubsystem.java
@@ -1,12 +1,20 @@
 package org.usfirst.frc4904.robot.subsystems;
 
 import org.usfirst.frc4904.standard.custom.motorcontrollers.SmartMotorController;
-import org.usfirst.frc4904.standard.custom.sensors.CustomEncoder;
+
+import edu.wpi.first.wpilibj2.command.Command;
 
 public class ShooterSubsystem extends MotorSubsystem {
-    public ShooterSubsystem(SmartMotorController shooterMotor, CustomEncoder shooterEncoder) {
+    public ShooterSubsystem(SmartMotorController shooterMotor1, SmartMotorController shooterMotor2) {
         super(
-            shooterMotor, 4
+            new SmartMotorController[] {shooterMotor1, shooterMotor2}, 4
         );
     }
+
+    public Command c_basicShoot(){
+        return c_forward(true);
+    }
+
+    
+
 }
