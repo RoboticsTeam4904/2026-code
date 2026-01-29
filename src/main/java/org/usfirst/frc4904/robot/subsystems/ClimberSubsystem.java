fda370d4b3c396b8f9dc9b882c6170f43a660d3b
Robby20914 <sofia.robison@gmail.com>
2026-01-28 17:11:35 -0800

    added intake subsystem (that works)

--- src/main/java/org/usfirst/frc4904/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/org/usfirst/frc4904/robot/subsystems/ClimberSubsystem.java
@@ -2,10 +2,12 @@ package org.usfirst.frc4904.robot.subsystems;
 
 import org.usfirst.frc4904.standard.custom.motorcontrollers.SmartMotorController;
 
+import edu.wpi.first.wpilibj.DutyCycle;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class ClimberSubsystem extends MotorSubsystem{
-    public ClimberSubsystem(SmartMotorController climbMotor, CustomEncoder climbEncoder){
+    public ClimberSubsystem(SmartMotorController climbMotor, DutyCycleEncoder climbEncoder){
         super(
             climbMotor, 4
         );
